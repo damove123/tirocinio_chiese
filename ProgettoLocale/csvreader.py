0eca875794737294349fe4fef9aba7fc6f5a6d7d
matteoDonaggio99 <879769@stud.unive.it>
2024-03-20 20:17:46 +0100

    csvreader corretto che corregge json

--- ProgettoLocale/csvreader.py
+++ ProgettoLocale/csvreader.py
@@ -1,13 +1,25 @@
-import csv
+import json
 
-# Define the path to your CSV file
-csv_file_path = '/Users/albi/Desktop/Università/tirocinio_chiese/fileExcel/Chiese.CSV'
+# Percorso al file JSON
+file_path = r"C:\Users\matte\OneDrive\Documenti\tirocinio_chiese\fileExcel\_RepertiPivi.json"
 
-# Open the CSV file in read mode
-with open(csv_file_path, 'r', encoding='latin-1') as file:
-    # Create a CSV reader object
-    csv_reader = csv.reader(file)
-    # Iterate over each row in the CSV file
-    for row in csv_reader:
-        # Process each row as needed
-        print(row)
+# Carica il JSON da file
+with open(file_path, 'r', encoding='utf-8') as file:
+    data = json.load(file)  # `data` ora è una lista di dizionari
+
+# Itera su ogni oggetto nell'array
+# Itera su ogni oggetto nell'array JSON
+for obj in data:
+    # Controlla se la chiave "Latino" esiste nell'oggetto corrente
+    if "Latino" in obj:
+        # Assicurati che il valore di "Latino" sia una stringa prima di chiamare replace
+        if isinstance(obj['Latino'], str):
+            obj['Latino'] = obj['Latino'].replace('\t', ' ')  # Sostituisci '\n' con '\t'
+        else:
+            print("Il valore di 'Latino' non è una stringa", type(obj['Latino']))
+
+# Salva il JSON modificato
+with open(file_path, 'w', encoding='utf-8') as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)
+
+print("Sostituzione completata.")
