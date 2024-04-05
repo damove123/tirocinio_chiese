1a5a3b6e396965af122aedab02c48b7db26bf4ab
Pietro Donega <115079900+Polli0@users.noreply.github.com>
2024-04-05 14:51:41 +0200

    stampa chiese su web app

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 import firebase_admin
 import json
+import flask
 
 from firebase_admin import db, credentials
+from flask import Flask, render_template, jsonify
 
 cred = credentials.Certificate("credentials.json")
-firebase_admin.initialize_app(cred, {"databaseURL": "https://floor-tiles-vpc-default-rtdb.europe-west1.firebasedatabase.app/" })
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://floor-tiles-vpc-default-rtdb.europe-west1.firebasedatabase.app/"})
 
-ref = db.reference("/Chiese").get()
-formatted_ref = json.dumps(ref, indent=4, ensure_ascii=False)
-print(formatted_ref)
+app = Flask(__name__)
 
+#ref = db.reference("/Chiese").get()
+#formatted_ref = json.dumps(ref, indent=4, ensure_ascii=False)
+#print(formatted_ref)
+
+
+@app.route("/")
+def homepage():
+    ref = db.reference("/Chiese").get()
+    formatted_ref = json.dumps(ref, indent=4, ensure_ascii=False)
+    return formatted_ref
+
+if __name__ == "__main__":
+    app.run()
