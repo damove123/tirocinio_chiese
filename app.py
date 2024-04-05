1e2f229c272ffb0b409dfe2e3588352c9ffd5292
matteoDonaggio99 <matteo.donaggio@gmail.com>
2024-04-05 17:05:57 +0200

    Ora ho aggiunto un filtro e trova la chiesa cercata.

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import firebase_admin
-import json
-import flask
-
 from firebase_admin import db, credentials
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred, {
-    "databaseURL": "https://floor-tiles-vpc-default-rtdb.europe-west1.firebasedatabase.app/"})
+    "databaseURL": "https://floor-tiles-vpc-default-rtdb.europe-west1.firebasedatabase.app/"
+})
 
 app = Flask(__name__)
 
-
-
-
+'''
 @app.route("/")
 def homepage():
     try:
@@ -26,6 +22,44 @@ def homepage():
     except Exception as e:
         app.logger.error(f"Errore durante il recupero dei dati: {e}")
         return jsonify({'error': 'Impossibile completare la richiesta'}), 500
+'''
+'''
+@app.route("/search")
+def search():
+    query = request.args.get("query")
+    if query:
+        try:
+            ref = db.reference("/Chiese")
+            chiese = ref.get()
+            if chiese:
+                filtered_chiese = [chiesa for chiesa in chiese if query.lower() in chiesa.get("Nome Locale della Chiesa", "").lower()]
+                return render_template("index.html", chiese=filtered_chiese, query=query)
+            else:
+                return render_template("index.html", chiese=None, query=query)
+        except Exception as e:
+            app.logger.error(f"Errore durante la ricerca delle chiese: {e}")
+            return jsonify({'error': 'Impossibile completare la ricerca'}), 500
+    else:
+        return render_template("index.html", chiese=None, query="")
+'''
+@app.route("/")
+def search_church():
+    try:
+        query = request.args.get('query')  # Ottieni il valore inserito nell'input di ricerca
+        ref = db.reference("/Chiese")
+        chiese = ref.get()
+
+        # Filtra le chiese che corrispondono alla query
+        results = [chiesa for chiesa in chiese if chiesa["Locale Nome della Chiesa"] == query]
+
+
+        if results:
+            return render_template("index.html", chiese=results, query=query)
+        else:
+            return render_template("index.html", chiese=None, query=query)
+    except Exception as e:
+        app.logger.error(f"Errore durante la ricerca della chiesa: {e}")
+        return jsonify({'error': 'Impossibile completare la ricerca'}), 500
 
 
 if __name__ == "__main__":
