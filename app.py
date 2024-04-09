b075c7389d1327a867da94ae98812986299785aa
matteoDonaggio99 <matteo.donaggio@gmail.com>
2024-04-09 16:34:05 +0200

    Ho aggiunto la mappa. Per il momento vuota

--- app.py
+++ app.py
@@ -9,24 +9,42 @@ firebase_admin.initialize_app(cred, {
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def search_church():
     try:
         query = request.args.get('query')  # Ottieni il valore inserito nell'input di ricerca
-        ref = db.reference("/Chiese")
-        chiese = ref.get()
+        codice_corrispondente = None
+        chiese_ref = db.reference("/Chiese")
+        chiese = chiese_ref.get() or []
 
-        # Filtra le chiese che corrispondono alla query
-        results = [chiesa for chiesa in chiese if chiesa["Locale Nome della Chiesa"] == query]
+        for chiesa in chiese:
+            if chiesa.get("Locale Nome della Chiesa") == query:
+                codice_corrispondente = chiesa.get("Codice Chiesa")
+                break
 
+        # Se il codice è stato trovato, procedi con il filtraggio basato su quel codice
+        resultsChiese = []
+        resultsReperti = []
+        if codice_corrispondente:
+            if chiese:  # Reuse chiese se è già popolato e non None
+                resultsChiese.extend([chiesa for chiesa in chiese if chiesa.get("Codice Chiesa") == codice_corrispondente])
 
-        if results:
-            return render_template("index.html", chiese=results, query=query)
+            refPivi = db.reference("/RepertiPivi").get() or []
+            resultsReperti.extend([pivi for pivi in refPivi if pivi.get("Codice Chiesa") == codice_corrispondente])
+
+            refPolo = db.reference("/RepertiPolo").get() or []
+            resultsReperti.extend([polo for polo in refPolo if polo.get("Codice Chiesa") == codice_corrispondente])
+
+        # Verifica se ci sono risultati prima di restituirli
+        if resultsChiese:
+            return render_template("index.html", chiese=resultsChiese, reperti=resultsReperti,  query=query)
         else:
-            return render_template("index.html", chiese=None, query=query)
+            return render_template("index.html", chiese=None, reperti=None, query=query)
     except Exception as e:
-        app.logger.error(f"Errore durante la ricerca della chiesa: {e}")
+        app.logger.error(f"Errore durante la ricerca: {e}")
         return jsonify({'error': 'Impossibile completare la ricerca'}), 500
+
 
 
 if __name__ == "__main__":
---
+++ map.py
@@ -0,0 +1,12 @@
+from arcgis.gis import GIS
+
+gis = GIS("https://www.arcgis.com", "matteo.donaggio_serendpt", "Mobi80100")
+# Ottieni la mappa utilizzando l'ID mappa
+mappa = gis.content.get("5ae37f33f52a4eefbc098507ddd9f92d")
+
+# Supponendo che tu abbia una lista di dizionari con i dati delle chiese
+# ogni elemento della lista ha latitudine, longitudine, nome, e altri dettagli
+chiese_data = [{"latitude": 45.4375, "longitude": 12.3358, "nome": "Chiesa di Venezia", "dettagli": "Alcuni dettagli qui"}]  # Esempio
+
+# Qui dovresti convertire i tuoi dati in un formato compatibile con ArcGIS, come un FeatureCollection o simile
+# Questo dipende dalla struttura dei tuoi dati e da come desideri visualizzarli sulla mappa
