cfc26528b51be2ec53823194aa0394b46ede5d7c
damove123 <otrebla1399@gmail.com>
2024-04-12 14:16:41 +0200

    Tolte parti sbagliate

--- app.py
+++ app.py
@@ -14,40 +14,6 @@ firebase_admin.initialize_app(cred, {
 
 app = Flask(__name__)
 
-'''
-@app.route("/")
-def homepage():
-    try:
-        ref = db.reference("/Chiese")
-        chiese = ref.get()
-        if chiese:
-            return render_template("index.html", chiese=chiese)
-        else:
-            return render_template("index.html", chiese=None)
-    except Exception as e:
-        app.logger.error(f"Errore durante il recupero dei dati: {e}")
-        return jsonify({'error': 'Impossibile completare la richiesta'}), 500
-'''
-'''
-@app.route("/search")
-def search():
-    query = request.args.get("query")
-    if query:
-        try:
-            ref = db.reference("/Chiese")
-            chiese = ref.get()
-            if chiese:
-                filtered_chiese = [chiesa for chiesa in chiese if query.lower() in chiesa.get("Nome Locale della Chiesa", "").lower()]
-                return render_template("index.html", chiese=filtered_chiese, query=query)
-            else:
-                return render_template("index.html", chiese=None, query=query)
-        except Exception as e:
-            app.logger.error(f"Errore durante la ricerca delle chiese: {e}")
-            return jsonify({'error': 'Impossibile completare la ricerca'}), 500
-    else:
-        return render_template("index.html", chiese=None, query="")
-'''
-
 
 @app.route("/")
 def search_church():
