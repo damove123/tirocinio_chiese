d4a6af649cf099572c53d94cbc3a4372841f05c3
matteoDonaggio99 <matteo.donaggio@gmail.com>
2024-04-12 14:16:26 +0200

    Tentativo di vedere le immagini con aws s3

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import firebase_admin
 from firebase_admin import db, credentials
 from flask import Flask, render_template, jsonify, request
+import boto3
+
+# Configura il client S3
+s3_client = boto3.client('s3')
+
+def get_image_urls(bucket_name, church_code):
+    prefix = f"Church_Photos/{church_code}/Artifacts/"
+    try:
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        if response['KeyCount'] == 0:
+            print(f"Nessun oggetto trovato con il prefisso {prefix}")
+        image_urls = [f"https://{bucket_name}.s3.amazonaws.com/{obj['Key']}" for obj in response.get('Contents', [])]
+        print(f"URLs trovate: {image_urls}")
+        return image_urls
+    except Exception as e:
+        print(f"Errore nel recupero delle immagini: {e}")
+        return []
+
+
 
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred, {
@@ -9,11 +28,10 @@ firebase_admin.initialize_app(cred, {
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def search_church():
     try:
-        query = request.args.get('query')  # Ottieni il valore inserito nell'input di ricerca
+        query = request.args.get('query')
         codice_corrispondente = None
         chiese_ref = db.reference("/Chiese")
         chiese = chiese_ref.get() or []
@@ -23,29 +41,29 @@ def search_church():
                 codice_corrispondente = chiesa.get("Codice Chiesa")
                 break
 
-        # Se il codice è stato trovato, procedi con il filtraggio basato su quel codice
         resultsChiese = []
         resultsReperti = []
         if codice_corrispondente:
-            if chiese:  # Reuse chiese se è già popolato e non None
-                resultsChiese.extend([chiesa for chiesa in chiese if chiesa.get("Codice Chiesa") == codice_corrispondente])
+            resultsChiese.extend([chiesa for chiesa in chiese if chiesa.get("Codice Chiesa") == codice_corrispondente])
 
             refPivi = db.reference("/RepertiPivi").get() or []
-            resultsReperti.extend([pivi for pivi in refPivi if pivi.get("Codice Chiesa") == codice_corrispondente])
+            for pivi in refPivi:
+                if pivi.get("Codice Chiesa") == codice_corrispondente:
+                    pivi['image_urls'] = get_image_urls('floor-tiles-vpc', pivi.get("Codice Reperto"))
+                    resultsReperti.append(pivi)
 
             refPolo = db.reference("/RepertiPolo").get() or []
-            resultsReperti.extend([polo for polo in refPolo if polo.get("Codice Chiesa") == codice_corrispondente])
+            for polo in refPolo:
+                if polo.get("Codice Chiesa") == codice_corrispondente:
+                    polo['image_urls'] = get_image_urls('floor-tiles-vpc', polo.get("Codice Reperto"))
+                    resultsReperti.append(polo)
 
-        # Verifica se ci sono risultati prima di restituirli
         if resultsChiese:
-            return render_template("index.html", chiese=resultsChiese, reperti=resultsReperti,  query=query)
+            return render_template("index.html", chiese=resultsChiese, reperti=resultsReperti, query=query)
         else:
             return render_template("index.html", chiese=None, reperti=None, query=query)
     except Exception as e:
-        app.logger.error(f"Errore durante la ricerca: {e}")
         return jsonify({'error': 'Impossibile completare la ricerca'}), 500
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
