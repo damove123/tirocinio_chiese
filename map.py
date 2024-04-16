5b256a0373d8516c8b4689405d2e2fd7cdba4930
matteoDonaggio99 <matteo.donaggio@gmail.com>
2024-04-16 09:23:56 +0200

    Commit per inizialre la giornata altrimenti non funziona niente

--- map.py
+++ map.py
@@ -6,7 +6,7 @@ mappa = gis.content.get("5ae37f33f52a4eefbc098507ddd9f92d")
 
 # Supponendo che tu abbia una lista di dizionari con i dati delle chiese
 # ogni elemento della lista ha latitudine, longitudine, nome, e altri dettagli
-chiese_data = [{"latitude": 45.4375, "longitude": 12.3358, "nome": "Chiesa di Venezia", "dettagli": "Alcuni dettagli qui"}]  # Esempio
+chiese_data = [{"latitude": 45.4375, "longitude": 12.3358, "nome": "Chiesa di Venezia", "dettagli": "Alcuni dettagli qui"}]
 
 # Qui dovresti convertire i tuoi dati in un formato compatibile con ArcGIS, come un FeatureCollection o simile
 # Questo dipende dalla struttura dei tuoi dati e da come desideri visualizzarli sulla mappa
