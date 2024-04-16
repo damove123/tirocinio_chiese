31f502f0947610b078079ff4485b6ea1c7730fe7
Pietro Donega <115079900+Polli0@users.noreply.github.com>
2024-04-16 12:04:19 +0200

    ricerche con margine di errore

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import db, credentials
 from flask import Flask, render_template, jsonify, request
+from fuzzywuzzy import fuzz
 import boto3
 
 cred = credentials.Certificate("credentials.json")
@@ -10,6 +11,18 @@ firebase_admin.initialize_app(cred, {
 
 # Configura il client S3
 s3_client = boto3.client('s3')
+
+
+def normalize_name(name):
+    if not name:
+        return ""  # Restituisci una stringa vuota se il nome è None o vuoto
+    name = name.replace("S.", "San").replace("St.", "Santo")
+    return name.strip()
+
+
+def is_match(query, target):
+    # Utilizza una soglia per determinare se considerare un match valido
+    return fuzz.ratio(query, target) > 80  # ad esempio, una soglia del 80%
 
 
 def get_image_urls(bucket_name, church_code, reperto_code):
@@ -38,13 +51,15 @@ app = Flask(__name__)
 @app.route("/")
 def search_church():
     try:
-        query = request.args.get('query')
+        raw_query = request.args.get('query')
+        query = normalize_name(raw_query)
         codice_corrispondente = None
         chiese_ref = db.reference("/Chiese")
         chiese = chiese_ref.get() or []
 
         for chiesa in chiese:
-            if chiesa.get("Locale Nome della Chiesa") == query:
+            chiesa_name = normalize_name(chiesa.get("Locale Nome della Chiesa"))
+            if is_match(query, chiesa_name):
                 codice_corrispondente = chiesa.get("Codice Chiesa")
                 break
 
@@ -72,7 +87,8 @@ def search_church():
         else:
             return render_template("index.html", chiese=None, reperti=None, query=query)
     except Exception as e:
-        return jsonify({'error': 'Impossibile completare la ricerca'}), 500
+        print("Errore durante la ricerca:", str(e))  # Aggiunge dettaglio sull'errore
+        return jsonify({'error': 'Impossibile completare la ricerca: ' + str(e)}), 500
 
 
 if __name__ == "__main__":
