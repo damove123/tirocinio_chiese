46ec61aeb107510b294e16b94136b0eb23739ff8
damove123 <otrebla1399@gmail.com>
2024-04-19 13:04:50 +0200

    messo login.py e inserito in pagina iniziale

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
+from flask_login import current_user
 from fuzzywuzzy import fuzz
 from flask_caching import Cache
 import csv
@@ -7,34 +7,7 @@ import data
 import re
 
 app = Flask(__name__)
-app.secret_key = 'Chiese2012!'  # Imposta una chiave segreta casuale
-login_manager = LoginManager(app)
-
-
-# Funzione per validare l'email
-def email_valido(email):
-    # Utilizza un'espressione regolare per controllare il formato dell'email Questo pattern corrisponde a un'email
-    # standard, ma potrebbe essere necessario adattarlo alle tue esigenze specifiche
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(pattern, email) is not None
-
-
-# Funzione per validare la password
-def password_valida(password):
-    # Controlla se la password ha almeno 8 caratteri
-    return len(password) >= 8
-
-
-# Definizione del modello utente
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
-
-
-# Funzione per recuperare un utente dall'ID
-@login_manager.user_loader
-def load_user(user_id):
-    return User(user_id)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 # Normalizza il nome
@@ -48,33 +21,6 @@ def normalize_name(name):
 # Verifica se c'è una corrispondenza tra la query e il target
 def is_match(query, target):
     return fuzz.ratio(query, target) > 80
-
-
-# Route per il login
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        # Esegui la logica di autenticazione qui
-        if email_valido(email) and password_valida(password):
-            user = User(id=email)  # Crea un oggetto utente
-            login_user(user)  # Effettua il login dell'utente
-            return redirect(url_for('search_church'))  # Reindirizza l'utente dopo il login
-        else:
-            flash('Email o password non valide', 'error')
-    return render_template('login.html')
-
-
-# Route per il logout
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()  # Effettua il logout dell'utente
-    return redirect(url_for('search_church'))  # Reindirizza l'utente alla homepage dopo il logout
-
-
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 def sub(string: str):
@@ -140,13 +86,15 @@ def search_church():
                 id.append(item['id'])
                 scritte.append(item['inscription'])
 
-            return render_template("result.html", chiesa=query, reperti=id, scritte=scritte, immagini=immagini, query=query)
+            return render_template("result.html", chiesa=query, reperti=id, scritte=scritte, immagini=immagini,
+                                   query=query)
 
         except Exception as e:
             print(f"Error in search_church: {str(e)}")
             return render_template("index.html", message="Errore durante il processo di ricerca.")
     else:
         return redirect(url_for('login'))  # Reindirizza l'utente alla pagina di login se non è autenticato
+
 
 if __name__ == "__main__":
     app.run(debug=True)
