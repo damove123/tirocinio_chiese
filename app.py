be36811404a717089fa8aab185377f0127c2505a
damove123 <otrebla1399@gmail.com>
2024-04-19 15:17:31 +0200

    aggiornato varie

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
-from flask_login import current_user
 from fuzzywuzzy import fuzz
 from flask_caching import Cache
 import csv
@@ -7,7 +7,34 @@ import data
 import re
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+app.secret_key = 'Chiese2012!'  # Imposta una chiave segreta casuale
+login_manager = LoginManager(app)
+
+
+# Funzione per validare l'email
+def email_valido(email):
+    # Utilizza un'espressione regolare per controllare il formato dell'email Questo pattern corrisponde a un'email
+    # standard, ma potrebbe essere necessario adattarlo alle tue esigenze specifiche
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
+
+
+# Funzione per validare la password
+def password_valida(password):
+    # Controlla se la password ha almeno 8 caratteri
+    return len(password) >= 8
+
+
+# Definizione del modello utente
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+
+# Funzione per recuperare un utente dall'ID
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 
 # Normalizza il nome
@@ -21,6 +48,33 @@ def normalize_name(name):
 # Verifica se c'è una corrispondenza tra la query e il target
 def is_match(query, target):
     return fuzz.ratio(query, target) > 80
+
+
+# Route per il login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        # Esegui la logica di autenticazione qui
+        if email_valido(email) and password_valida(password):
+            user = User(id=email)  # Crea un oggetto utente
+            login_user(user)  # Effettua il login dell'utente
+            return redirect(url_for('search_church'))  # Reindirizza l'utente dopo il login
+        else:
+            flash('Email o password non valide', 'error')
+    return render_template('login.html')
+
+
+# Route per il logout
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()  # Effettua il logout dell'utente
+    return redirect(url_for('search_church'))  # Reindirizza l'utente alla homepage dopo il logout
+
+
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 def sub(string: str):
@@ -79,7 +133,6 @@ def search_church():
             for artifact in artifact_url:
                 reperti.append(artifact)
 
-            print(reperti)
             cleanData = [seperator(value) for value in reperti]
 
             for item in cleanData:
@@ -87,8 +140,7 @@ def search_church():
                 id.append(item['id'])
                 scritte.append(item['inscription'])
 
-            return render_template("result.html", chiesa=query, reperti=id, scritte=scritte, immagini=immagini,
-                                   query=query)
+            return render_template("result.html", chiesa=query, reperti=id, scritte=scritte, immagini=immagini, query=query)
 
         except Exception as e:
             print(f"Error in search_church: {str(e)}")
@@ -96,6 +148,41 @@ def search_church():
     else:
         return redirect(url_for('login'))  # Reindirizza l'utente alla pagina di login se non è autenticato
 
+def formatta_nome(codice_reperto):
+    uppercase_letters = codice_reperto.split('_')[0]
+    # Aggiunge "%20Artifacts" alla stringa di lettere maiuscole
+    result_string = uppercase_letters + "%20Artifacts"
+    return result_string
+
+
+@app.route("/search_reperto", methods=["GET"])
+def search_reperto():
+    query = request.args.get('query')
+    if not query:
+        return render_template("index.html", message="Inserisci il codice del reperto.")
+
+    reperto_url = None
+    reperto_scritte = None
+    try:
+        codice_reperto = normalize_name(query)
+        artifact_group = formatta_nome(codice_reperto)
+        ck_id_list = data.getGroup(artifact_group)
+        dati_reperti = data.getData(ck_id_list)
+        for reperto in dati_reperti:
+            if reperto["data_Artifact Code"] == codice_reperto:
+                # Gestisce la possibilità che l'immagine non sia disponibile
+                reperto_url = reperto.get("media0_medium", None)
+                reperto_scritte = reperto.get("data_Transcription", None)
+                break  # Esce dal ciclo una volta trovato il reperto corrispondente
+
+        if reperto_url or reperto_scritte:  # Verifica se abbiamo trovato dati utili
+            return render_template("clickReperto.html", reperto=codice_reperto, scritte=reperto_scritte, immagini=reperto_url, query=query)
+        else:
+            return render_template("index.html", message="Nessun dato disponibile per il codice inserito.")
+
+    except Exception as e:
+        print("Errore:", e)
+        return render_template("index.html", message="Errore nel processo di ricerca. Assicurati che il codice del reperto sia valido.")
 
 if __name__ == "__main__":
     app.run(debug=True)
