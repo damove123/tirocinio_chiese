86c197c0de8d4b60a98834bfa190a43a44d530ea
damove123 <otrebla1399@gmail.com>
2024-05-02 10:51:26 +0200

    Risolto nomi rotti

--- app.py
+++ app.py
@@ -12,18 +12,8 @@ app = Flask(__name__)
 dati_reperti = None
 
 
-# Funzione per validare l'email
-def email_valido(email):
-    # Utilizza un'espressione regolare per controllare il formato dell'email Questo pattern corrisponde a un'email
-    # standard, ma potrebbe essere necessario adattarlo alle tue esigenze specifiche
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return re.match(pattern, email) is not None
-
-
-# Funzione per validare la password
-def password_valida(password):
-    # Controlla se la password ha almeno 8 caratteri
-    return len(password) >= 8
+def split_name(name):
+    return name.replace('Church Floor Artifact - ', '')
 
 
 def trova_miglior_corrispondenza(nome_chiesa, path_file='Churches.csv'):
@@ -74,15 +64,14 @@ def search_church():
         closest_match = churches['Local Name'].apply(lambda x: SequenceMatcher(None, x, query).ratio()).idxmax()
         church_info = churches.iloc[closest_match]
 
-
         church_data = {
             'local_name': church_info['Local Name'],
-             'full_name': church_info['Full Name'],
-             'year_founded': church_info['Year Founded'],
-             'intro_sentence': church_info['Intro sentence'],
-             'history_blurb': church_info['History Blurb'],
-             'longitude': church_info['Longitude Coordinate'],
-             'latitude': church_info['Latitude Coordinate'],
+            'full_name': church_info['Full Name'],
+            'year_founded': church_info['Year Founded'],
+            'intro_sentence': church_info['Intro sentence'],
+            'history_blurb': church_info['History Blurb'],
+            'longitude': church_info['Longitude Coordinate'],
+            'latitude': church_info['Latitude Coordinate'],
         }
 
         # Estrae i dati necessari dai reperti correlati alla chiesa trovata
@@ -105,11 +94,12 @@ def search_church():
 
             cleanData = [seperator(value) for value in dati_reperti]
             immagini = [item['url'] for item in cleanData]
-            id = [item['id'] for item in cleanData]
+            id = [split_name(item['id'])for item in cleanData]
             scritte = [item['inscription'] for item in cleanData]
 
         # Passa le informazioni della chiesa e i dati dei reperti al template
-        return render_template("result.html", church_data=church_data, reperti=id, scritte=scritte, immagini=immagini, query=query)
+        return render_template("result.html", church_data=church_data, reperti=id, scritte=scritte, immagini=immagini,
+                               query=query)
     except Exception as e:
         print(f"Error in search_church: {str(e)}")
         return render_template("index.html", message="Errore durante il processo di ricerca.")
@@ -130,7 +120,7 @@ def search_reperto():
 
     reperto_traduz = None
     reperto_id = None
-    reperto_url = None
+    reperto_url = '/static/noimage.jpg'
     reperto_scritte = None
     reperto_condition = None
     reperto_length = None
@@ -162,10 +152,10 @@ def search_reperto():
                 break  # Esce dal ciclo una volta trovato il reperto corrispondente
 
         if request.method == "POST":
-                    reperto_traduz = request.form['NEWtranslation']
-                    email = request.form['emailTK']
-                    uid = request.form['uidTK']
-                    data.update_translation(reperto_id, reperto_traduz,email,uid)
+            reperto_traduz = request.form['NEWtranslation']
+            email = request.form['emailTK']
+            uid = request.form['uidTK']
+            data.update_translation(reperto_id, reperto_traduz, email, uid)
 
         if reperto_url or reperto_scritte:  # Verifica se abbiamo trovato dati utili
             return render_template("clickReperto.html", reperto=query, scritte=reperto_scritte,
